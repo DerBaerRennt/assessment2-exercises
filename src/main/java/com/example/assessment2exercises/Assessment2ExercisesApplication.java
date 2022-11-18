ca507c6457059c0f6cd97848ffff7a484c63f566
DerBaerRennt <beerenboom@web.de>
2022-11-18 14:50:56 +0100

    - nur die "unversioned files"

---
+++ src/main/java/com/example/assessment2exercises/Assessment2ExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.example.assessment2exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assessment2ExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assessment2ExercisesApplication.class, args);
+    }
+
+}
